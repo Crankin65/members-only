e235344fd96dc00585969117e854b5436616c62c
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-09-11 22:18:00 -0400

    Updated post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,11 +1,25 @@
 class PostController < ApplicationController
+  before_action :require_login, only: [:new, :create]
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(user_id = params[:user_id])
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def require_login
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user, :user_id)
   end
 end

ee2b2633313b62a3c0d36d38fafe000ebc8c349d
Crankin65 <44421748+Crankin65@users.noreply.github.com>
2022-09-14 16:45:37 -0400

    Updated controllers with flash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_195505) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_185142) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_195505) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
